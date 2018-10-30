e764da3281d121a4d08f64f4a9275313c0e72b4d
phiratio <phiratio161@gmail.com>
2018-10-31 00:01:01 +0200

    31.10.2018 siick css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import StorePicker from "./components/StorePicker.jsx"
+import StorePicker from "./components/StorePicker.jsx";
+import "./css/style.css"
 
 
 render(<StorePicker/>, document.querySelector('#main') );
