c547efe1ea7abf19dc72b305b180940660f02dde
phiratio <phiratio161@gmail.com>
2018-10-30 23:27:59 +0200

    31.10.2018 moved from js to jsx

--- src/components/StorePicker.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class StorePicker extends React.Component {
-    render() {
-        return <p>Store picker will be here ;)</p>;
-    }
-}
-
-export default  StorePicker;
