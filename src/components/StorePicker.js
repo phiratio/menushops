21356f0753e219cc7a9f8b931a22b672ff118784
phiratio <phiratio161@gmail.com>
2018-10-30 21:44:53 +0200

    30.10.2018 first component - StorePicker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>Store picker will be here ;)</p>;
+    }
+}
+
+export default  StorePicker;
