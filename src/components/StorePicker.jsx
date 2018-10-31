a2520d31eac5bf6da1d5ac817c185413336c80f3
phiratio <phiratio161@gmail.com>
2018-10-31 14:33:48 +0200

    31.10.2018 :exclamation: this binding showcase with arrow function

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  // Okay this is how we bind the methods to this
-  constructor() {
-    super();
-    this.goToStore = this.goToStore.bind(this); // this inside the gotostore is now the storepicker's context
-}
+
 
   myInput = React.createRef();
 
-  goToStore(event) {
+  goToStore =(event) => {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Get the text from the input
+    console.log(this.myInput)
     // 3. Change the page to /store/:storename
-  }
+  };
 
   render() {
     return (
