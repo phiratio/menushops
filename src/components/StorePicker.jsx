24b9d6567bfa76f1d4f0a4d8e5413fc924224129
phiratio <phiratio161@gmail.com>
2018-10-31 13:36:20 +0200

    31.10.2018 :exclamation: this binding showcase with native es6 clases

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import {getFunName} from "../helpers";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <form className="store-selector">
-                    <h2>Please Enter A store</h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
-                    <button type="submit">Visit Store</button>
-                </form>
-            </React.Fragment>
-        )
-    }
+  // Okay this is how we bind the methods to this
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this); // this inside the gotostore is now the storepicker's context
 }
 
-export default  StorePicker;
+  myInput = React.createRef();
+
+  goToStore(event) {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from the input
+    // 3. Change the page to /store/:storename
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please Enter A store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StorePicker;
