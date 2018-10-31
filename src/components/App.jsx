cc00cba2dd0abf404d32d07a11060b434661faf3
phiratio <phiratio161@gmail.com>
2018-10-31 12:27:48 +0200

    31.10.2018 react router added, 3 routes added :heavy_check_mark:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
-import Order from "./Order"
+import Order from "./Order";
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline={"Fresh Seafood Market"}/>
+          <Header tagline={"Fresh Seafood Market"} />
         </div>
         <Order />
         <Inventory />
