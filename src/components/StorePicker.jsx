00c3187da99e9cef237cdba880b7ce6f891962a5
phiratio <phiratio161@gmail.com>
2018-10-31 14:47:49 +0200

    31.10.2018 changing webpage adress via react router and onclick event

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-
-
   myInput = React.createRef();
 
-  goToStore =(event) => {
+  goToStore = event => {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Get the text from the input
-    console.log(this.myInput)
+    const storeName = this.myInput.current.value;
     // 3. Change the page to /store/:storename
+    this.props.history.push(`/store/${storeName}`);
   };
 
   render() {
