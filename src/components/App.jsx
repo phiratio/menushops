ac9ab417f43ad2fa0380e173efcf795e702c103b
phiratio <phiratio161@gmail.com>
2018-11-01 03:41:09 +0200

    01.11.2018 :fire: if you need to accesss key, you must pass it second time!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,6 +28,14 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
+  addToOrder = (key) => {
+    // 1. take a copy of state
+    const order = {...this.state.order};
+    // 2. either add to the order, or update the number of the order
+    order[key] = order[key] + 1 || 1;
+    // 3. call setState to update our state object
+    this.setState({order});
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +43,7 @@ class App extends React.Component {
           <Header tagline={"Fresh Seafood Market"} />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key =>
-              <Fish key={key} details={this.state.fishes[key]}/>
+              <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
             )}
           </ul>
         </div>
--- src/components/Fish.jsx
+++ src/components/Fish.jsx
@@ -4,13 +4,14 @@ import {formatPrice} from "../helpers";
 class Fish extends React.Component {
   render() {
     const {image, name, price, desc, status} = this.props.details;
+    const isAvailable = status === "available";
     return <li className="menu-fish">
       <img src={image} alt={name}/>
       <h3 className="fish-name">{name}
       <span className="price">{formatPrice(price)}</span>
       </h3>
       <p>{desc}</p>
-      <button>Add To Cart</button>
+      <button onClick={this.props.addToOrder()} disabled={!isAvailable}>{isAvailable ? "Add to cart":"Sold out!"}</button>
     </li>;
   }
 }
