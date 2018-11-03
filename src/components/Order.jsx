b737981b3cae3959e9f965a5554fc6181cf0905a
phiratio <phiratio161@gmail.com>
2018-11-03 02:01:23 +0200

    03.11.2018 Order and fishes passed from app to order components

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 class Order extends React.Component {
-    render () {
-        return (
-            <div className="order">order!</div>
-        )
-    }
+  render() {
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+      </div>
+    );
+  }
 }
 
 export default Order;
