ef454a243088419cbd7da6471d34d563eeaf3412
phiratio <phiratio161@gmail.com>
2018-11-03 02:02:39 +0200

    03.11.2018 :exclamation: :exclamation: Whole state passed from App to Order

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,9 +52,7 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order
-          fishes={this.state.fishes}
-          order={this.state.order}/>
+        <Order {...this.state}/>
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
