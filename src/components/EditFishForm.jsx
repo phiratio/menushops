b6ed793da0fb9ab7ab6246b56814edb8391bdc51
phiratio <phiratio161@gmail.com>
2018-11-07 20:37:36 +0200

    Bi-directional data flow start

---
+++ src/components/EditFishForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class EditFishForm extends React.Component {
+  render() {
+    return (
+      <div className={"fish-edit"}>
+        <input type="text" name="name" value={this.props.fish.name} />
+        <input type="text" name="price" value={this.props.fish.price} />
+        <select name="status" value={this.props.fish.status}>
+          <option value="available">Fresh</option>
+          <option value="unavailable">Sold out</option>
+        </select>
+        <textarea name="desc" value={this.props.fish.desc} />
+        <input type="text" name="image" value={this.props.fish.image} />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
