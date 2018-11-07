d0e7c162035205b14c0d970c7c54ac14b756a1da
phiratio <phiratio161@gmail.com>
2018-11-07 21:09:23 +0200

    Bi-directional data flow pt2

--- src/components/EditFishForm.jsx
+++ src/components/EditFishForm.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 
 class EditFishForm extends React.Component {
+  handleChange = (event) => {
+    // 1. Take a copy of the current fish
+    const updatedFish = {...this.props.fish,
+    [event.currentTarget.name]: event.currentTarget.value};//computed props ES6
+    this.props.updateFish(this.props.index,updatedFish);
+  };
   render() {
     return (
       <div className={"fish-edit"}>
-        <input type="text" name="name" value={this.props.fish.name} />
-        <input type="text" name="price" value={this.props.fish.price} />
-        <select name="status" value={this.props.fish.status}>
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select
+          name="status"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
           <option value="available">Fresh</option>
           <option value="unavailable">Sold out</option>
         </select>
-        <textarea name="desc" value={this.props.fish.desc} />
-        <input type="text" name="image" value={this.props.fish.image} />
+        <textarea
+          name="desc"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
       </div>
     );
   }
